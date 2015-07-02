0f3c4e811ce81bc79e1ed4b080c7615a44c22546
Jan Kotanski <jankotan@gmail.com>
2015-07-02 10:16:04 +0200

    adding tests for datasource describer

--- test/DescriberTest.py
+++ test/DescriberTest.py
@@ -103,8 +103,12 @@ class DescriberTest(unittest.TestCase):
         self._bfloat = "float64" if IS64BIT else "float32"
 
         self.mydss = {
+            'nn': ('<definition><datasource type="TANGO">'
+                    '</datasource></definition>'), 
+            'nn2': ('<definition><datasource type="TANGO" name="">'
+                    '</datasource></definition>'), 
             'ann': ('<definition><datasource type="TANGO" name="ann">'
-                    '</datasource></definition>'),
+                    '</datasource></definition>'), 
             'ann2': ('<definition><datasource type="CLIENT" name="ann2">'
                      '</datasource></definition>'),
             'ann3': ('<definition><datasource type="DB" name="ann3">'
@@ -131,9 +135,43 @@ class DescriberTest(unittest.TestCase):
                      '<record name="myattr2"/>'
                      '<device member="attribute" name="dsf/sd/we"/>'
                      '</datasource></definition>'),
+            'P1M_postrun': (
+                '<definition>'
+                '<datasource type="PYEVAL" name="P1M_postrun">'
+                '<result name="result">'
+                'ds.result = "" + ds.P1M_fileDir + "/" + ds.P1M_filePrefix + "%03i" + ds.P1M_filePostfix + ":1:" +  str(ds.P1M_fileStartNum)</result>'
+                ' $datasources.P1M_fileStartNum'
+                ' $datasources.P1M_fileDir'
+                ' $datasources.P1M_filePostfix'
+                ' $datasources.P1M_filePrefix</datasource>'
+                '</definition>'),
+            'dbtest': (
+                '<definition>'
+                '<datasource type="DB" name="dbtest">'
+                '<database dbtype="MYSQL"/>'
+                '<query format="SPECTRUM">select name for device;</query>'
+                '</datasource>'
+                '</definition>'),
+            'dbds': (
+                '<definition>'
+                '<datasource type="DB">'
+                '<database dbtype="MYSQL">complicated DSN string</database>'
+                '<query format="IMAGE">select * from device</query>'
+                '<doc>test database datasource</doc>'
+                '</datasource>'
+                '</definition>'),
+            'slt1vgap': (
+                '<definition>'
+                '<datasource type="CLIENT" name="slt1vgap">'
+                '<record name="p02/slt/exp.07"/>'
+                '</datasource>'
+                '</definition>'
+                ),
            }
 
-        self.resdss = {
+        self.resdss = { 
+            'nn': ("nn", "TANGO", ""),
+            'nn2': ("", "TANGO", ""),
             'ann': ("ann", "TANGO", ""),
             'ann2': ("ann2", "CLIENT", ""),
             'ann3': ("ann3", "DB", ""),
@@ -143,6 +181,10 @@ class DescriberTest(unittest.TestCase):
             'tann1': ("tann1", "TANGO", "sfa:10000/dsf/myattr2"),
             'tann1b': ("tann1b", "TANGO", "dsf/myattr2"),
             'tann1c': ("tann1c", "TANGO", "dsf/sd/we/myattr2"),
+            'P1M_postrun':('P1M_postrun', "PYEVAL", ""),
+            'dbtest':('dbtest', "DB", ""),
+            'dbds':('dbds', "DB", ""),
+            'slt1vgap':('slt1vgap', "CLIENT", "p02/slt/exp.07"),
             }
 
     ## test starter
@@ -201,6 +243,32 @@ class DescriberTest(unittest.TestCase):
         res = des.dataSources(["ann"], "CLIENT")
         self.checkDS(res, [])
 
+
+    ## constructor test
+    # \brief It tests default settings
+    def test_datasources_server(self):
+        fun = sys._getframe().f_code.co_name
+        print "Run: %s.%s() " % (self.__class__.__name__, fun)
+        dsdict = {
+            "ann": self.mydss["ann"]
+            }
+        server = Server()
+        server.dsdict = dsdict
+        des = Describer(server)
+        self.assertEqual(des.dataSources(["myds2"]), {})
+
+        des = Describer(server)
+        res = des.dataSources(["ann"])
+        self.checkDS(res, ["ann"])
+
+        des = Describer(server)
+        res = des.dataSources(["ann"], "TANGO")
+        self.checkDS(res, ["ann"])
+
+        des = Describer(server)
+        res = des.dataSources(["ann"], "CLIENT")
+        self.checkDS(res, [])
+
     ## constructor test
     # \brief It tests default settings
     def test_datasources_noargs(self):
@@ -208,6 +276,19 @@ class DescriberTest(unittest.TestCase):
         print "Run: %s.%s() " % (self.__class__.__name__, fun)
 
         server = NoServer()
+        server.dsdict = self.mydss
+
+        des = Describer(server)
+        res = des.dataSources()
+        self.checkDS(res, self.resdss.keys())
+
+    ## constructor test
+    # \brief It tests default settings
+    def test_datasources_noargs_server(self):
+        fun = sys._getframe().f_code.co_name
+        print "Run: %s.%s() " % (self.__class__.__name__, fun)
+
+        server = Server()
         server.dsdict = self.mydss
 
         des = Describer(server)
@@ -229,5 +310,130 @@ class DescriberTest(unittest.TestCase):
             res,
             [k for k in self.resdss.keys() if self.resdss[k][1] == 'TANGO'])
 
+
+        des = Describer(server)
+        res = des.dataSources(dstype="CLIENT")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'CLIENT'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="DB")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'DB'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="PYEVAL")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'PYEVAL'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="NEW")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'NEW'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="UNKNOWN")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'UNKNOWN'])
+
+    ## constructor test
+    # \brief It tests default settings
+    def test_datasources_dstype_server(self):
+        fun = sys._getframe().f_code.co_name
+        print "Run: %s.%s() " % (self.__class__.__name__, fun)
+
+        server = Server()
+        server.dsdict = self.mydss
+
+        des = Describer(server)
+        res = des.dataSources(dstype="TANGO")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'TANGO'])
+
+
+        des = Describer(server)
+        res = des.dataSources(dstype="CLIENT")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'CLIENT'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="DB")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'DB'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="PYEVAL")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'PYEVAL'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="NEW")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'NEW'])
+
+        des = Describer(server)
+        res = des.dataSources(dstype="UNKNOWN")
+        self.checkDS(
+            res,
+            [k for k in self.resdss.keys() if self.resdss[k][1] == 'UNKNOWN'])
+
+
+    ## constructor test
+    # \brief It tests default settings
+    def test_datasources_names(self):
+        fun = sys._getframe().f_code.co_name
+        print "Run: %s.%s() " % (self.__class__.__name__, fun)
+
+        server = NoServer()
+        server.dsdict = self.mydss
+
+        names_list = [
+            [],
+            ["ann3"],
+            ["ann", "nn2", "tann0", "dbtest", "slt1vgap"],
+            [ 'nn', 'nn2', 'ann', 'ann2', 'ann3', 'ann4', 'ann5', 'tann0', 'tann1',
+              'tann1b', 'tann1c', 'P1M_postrun', 'dbtest', 'dbds', 'slt1vgap']
+            ]
+
+        for names in names_list:
+            des = Describer(server)
+            res = des.dataSources(names)
+            self.checkDS(res, names)
+            
+    ## constructor test
+    # \brief It tests default settings
+    def test_datasources_names_server(self):
+        fun = sys._getframe().f_code.co_name
+        print "Run: %s.%s() " % (self.__class__.__name__, fun)
+
+        server = Server()
+        server.dsdict = self.mydss
+
+        names_list = [
+            [],
+            ["ann3"],
+            ["ann", "nn2", "tann0", "dbtest", "slt1vgap"],
+            [ 'nn', 'nn2', 'ann', 'ann2', 'ann3', 'ann4', 'ann5', 'tann0', 'tann1',
+              'tann1b', 'tann1c', 'P1M_postrun', 'dbtest', 'dbds', 'slt1vgap']
+            ]
+
+        for names in names_list:
+            des = Describer(server)
+            res = des.dataSources(names)
+            self.checkDS(res, names)
+            
+
+
 if __name__ == '__main__':
     unittest.main()
+
