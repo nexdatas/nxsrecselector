22b2592d32bceb8f29eeef0928ba5a7de0395c91
Jan Kotanski <jankotan@gmail.com>
2017-03-18 09:53:55 +0100

    fix result

--- test/runtest.py
+++ test/runtest.py
@@ -180,7 +180,7 @@ def main():
     
     ## test result
     result = runner.run(suite).wasSuccessful()
-    sys.exit(result)
+    sys.exit(not result)
     
 if __name__ == "__main__":
     main()
