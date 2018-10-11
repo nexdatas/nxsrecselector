b92557366823c921524044ee3e100a701ae77566
Jan Kotanski <jankotan@gmail.com>
2018-10-11 11:08:01 +0200

    extent tests

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-echo "run python-nxsrecselector"
-docker exec -it ndts python test/runtest.py $1
+if [ $2 = "2" ]; then
+    echo "run python-nxsrecselector"
+    docker exec -it ndts python test/runtest.py $1
+else
+    echo "run python3-nxsrecselector"
+    docker exec -it ndts python3 test/runtest.py $1
+fi
 if [ $? -ne "0" ]
 then
     exit -1
