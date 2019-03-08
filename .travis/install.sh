f6030806ae2c237ef1446a7b685ca12d57a03aa7
Jan Kotanski <jankotan@gmail.com>
2019-03-08 13:27:18 +0100

    update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,8 +21,6 @@ then
     exit -1
 fi
 
-docker exec -it --user root ndts service tango-db restart
-docker exec -it --user root ndts service tango-starter restart
 
 if [ $2 = "2" ]; then
     echo "install python-pytango"
@@ -30,10 +28,12 @@ if [ $2 = "2" ]; then
 else
     echo "install python3-pytango"
     docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y git python3-six python3-numpy graphviz python3-sphinx g++ build-essential python3-dev pkg-config python3-all-dev  python3-setuptools libtango-dev python3-pytango python3-tz python-pytango python-enum34; apt-get -qq install -y nxsconfigserver-db; sleep 10'
+    docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python-dev libboost-dev'
     if [ $1 = "debian10" ]; then
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python1.62-dev libboost1.62-dev'
+	# docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python1.62-dev libboost1.62-dev'
     else
-	docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python-dev libboost-dev'
+	docker exec -it --user root ndts service tango-db restart
+	docker exec -it --user root ndts service tango-starter restart
     fi
 
 fi
