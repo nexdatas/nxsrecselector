faddc66c7818dd26c74960364658a1ba038fc8bf
Jan Kotanski <jankotan@gmail.com>
2021-09-24 21:18:01 +0200

    change install order

--- .ci/install.sh
+++ .ci/install.sh
@@ -33,18 +33,18 @@ docker exec --user root ndts service tango-starter restart
 
 if [ "$2" = "2" ]; then
     echo "install python-pytango"
-    docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-setuptools python-enum34 python-pytango python-tz; apt-get -qq install -y nxsconfigserver-db; sleep 10'
+    docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y  python-setuptools python-enum34 python-pytango python-tz; apt-get -qq install -y nxsconfigserver-db; sleep 10'
 else
     echo "install python3-pytango"
-    docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python-dev libboost-dev'
+    docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python-dev libboost-dev git python3-six python3-numpy graphviz python3-sphinx g++ build-essential python3-dev pkg-config python3-all-dev  python3-setuptools libtango-dev python3-tz python3-enum34'
     if [ "$1" = "debian10" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "debian11" ]; then
 	echo " "
-	docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y git python3-six python3-numpy graphviz python3-sphinx g++ build-essential python3-dev pkg-config python3-all-dev  python3-setuptools libtango-dev python3-tz python3-tango python3-enum34; apt-get -qq install -y nxsconfigserver-db; sleep 10'
-	# docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python1.62-dev libboost1.62-dev'
+	docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y python3-tango; apt-get -qq install -y nxsconfigserver-db; sleep 10'
+	# docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y libboost-python1.62-dev libboost1.62-dev'
     else
-	docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y git python3-six python3-numpy graphviz python3-sphinx g++ build-essential python3-dev pkg-config python3-all-dev  python3-setuptools libtango-dev python3-tz python3-pytango python3-enum34; apt-get -qq install -y nxsconfigserver-db; sleep 10'
-	docker exec --user root ndts /bin/sh -c 'git clone https://gitlab.com/tango-controls/pytango pytango; cd pytango; git checkout tags/v9.2.5 -b b9.2.5'
-	docker exec --user root ndts /bin/sh -c 'cd pytango; python3 setup.py install'
+	docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y python3-pytango; apt-get -qq install -y nxsconfigserver-db; sleep 10'
+	docker exec --user root ndts /bin/bash -c 'git clone https://gitlab.com/tango-controls/pytango pytango; cd pytango; git checkout tags/v9.2.5 -b b9.2.5'
+	docker exec --user root ndts /bin/bash -c 'cd pytango; python3 setup.py install'
     fi
 
 fi
@@ -54,16 +54,16 @@ if [ "$?" -ne "0" ]; then exit 255; fi
 if [ "$1" = "debian8" ]; then
     if [ "$2" = "3" ]; then
 	echo "install python3-mysqldb"
-	docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y -t=jessie-backports  python3-mysqldb'
+	docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y -t=jessie-backports  python3-mysqldb'
     fi
 fi
 
 if [ "$2" = "2" ]; then
     echo "install sardana, taurus and nexdatas"
-    docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  python-nxsconfigserver python-nxswriter python-nxstools'
+    docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  python-nxsconfigserver python-nxswriter python-nxstools'
 else
     echo "install sardana, taurus and nexdatas"
-    docker exec --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  python3-nxsconfigserver python3-nxswriter python3-nxstools'
+    docker exec --user root ndts /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get install -y  python3-nxsconfigserver python3-nxswriter python3-nxstools'
 fi
 if [ "$?" -ne "0" ]; then exit 255; fi
 
