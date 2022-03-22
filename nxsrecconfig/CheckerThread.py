130a93b0cb86dae84e812c75da2584502eaff521
Jan Kotanski <jankotan@gmail.com>
2022-03-22 16:08:08 +0100

    Issue 51: add OffStateError (#53)
    * switch off a component if the corresponding device is off
    
    * trigger build
    
    * add tests
    
    * add OffState
    
    * apply pep8
    
    * add checks for DISABLE, INIT, INSERT, CLOSE, UNKNOWN states

--- nxsrecconfig/CheckerThread.py
+++ nxsrecconfig/CheckerThread.py
@@ -137,10 +137,15 @@ class CheckerThread(threading.Thread):
                 TangoUtils.wait(dp, state=None)
                 dp.set_timeout_millis(10000)
                 state = dp.state()
-                if state in [PyTango.DevState.FAULT]:
-                    raise FaultStateError("FAULT STATE")
-                if state in [PyTango.DevState.OFF]:
-                    raise FaultStateError("OFF STATE")
+                if state in [PyTango.DevState.FAULT,
+                             PyTango.DevState.DISABLE]:
+                    raise FaultStateError("%s STATE" % state)
+                if state in [PyTango.DevState.OFF,
+                             PyTango.DevState.INIT,
+                             PyTango.DevState.INSERT,
+                             PyTango.DevState.CLOSE,
+                             PyTango.DevState.UNKNOWN]:
+                    raise OffStateError("%s STATE" % state)
                 dp.ping()
                 if not ds.attr:
                     for gattr in ATTRIBUTESTOCHECK:
@@ -185,4 +190,10 @@ class AlarmStateError(Exception):
 class FaultStateError(Exception):
 
     """ Fault State Exception class
+    """
+
+
+class OffStateError(Exception):
+
+    """ Off State Exception class
     """
