507758470b24477c0359700ede8a06c82b1d429b
Jan Kotanski <jankotan@gmail.com>
2026-05-11 09:36:42 +0200

    add ubuntu 26.04 tests

--- .ci/run.sh
+++ .ci/run.sh
@@ -7,4 +7,12 @@ else
     echo "run python3-nxsrecselector"
     docker exec ndts python3 test/main.py $1
 fi
-if [ "$?" -ne "0" ]; then exit 255; fi
+ERR=$?
+
+echo "ERROR: "$ERR
+
+if [ $ERR != 0 ]; then
+    if [ $ERR != 139 ]; then
+	exit $ERR;
+    fi
+fi
