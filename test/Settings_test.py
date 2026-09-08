f865c2e8ebd4d729cde35db3f86e8b44ca3346a5
Jan Kotanski <jankotan@gmail.com>
2026-09-08 16:28:37 +0200

    add support for pymysql

--- test/Settings_test.py
+++ test/Settings_test.py
@@ -95,31 +95,43 @@ class NotEqualException(Exception):
 
 
 try:
-    import MySQLdb
+    try:
+        import MySQLdb
+    except ImportError:
+        import pymysql as MySQLdb
+        MySQLdb.install_as_MySQLdb()
     # connection arguments to MYSQL DB
     mydb = MySQLdb.connect({})
     mydb.close()
     DB_AVAILABLE.append("MYSQL")
 except Exception:
     try:
-        import MySQLdb
-    # connection arguments to MYSQL DB
+        try:
+            import MySQLdb
+        except ImportError:
+            import pymysql as MySQLdb
+            MySQLdb.install_as_MySQLdb()
+        # connection arguments to MYSQL DB
         args = {'db': 'nxsconfig',
                 'read_default_file': '/etc/my.cnf', 'use_unicode': True}
-    # inscance of MySQLdb
+        # inscance of MySQLdb
         mydb = MySQLdb.connect(**args)
         mydb.close()
         DB_AVAILABLE.append("MYSQL")
     except Exception:
         try:
-            import MySQLdb
+            try:
+                import MySQLdb
+            except ImportError:
+                import pymysql as MySQLdb
+                MySQLdb.install_as_MySQLdb()
             from os.path import expanduser
             home = expanduser("~")
-        # connection arguments to MYSQL DB
+            # connection arguments to MYSQL DB
             args2 = {'db': 'nxsconfig',
                      'read_default_file': '%s/.my.cnf' % home,
                      'use_unicode': True}
-        # inscance of MySQLdb
+            # inscance of MySQLdb
             mydb = MySQLdb.connect(**args2)
             mydb.close()
             DB_AVAILABLE.append("MYSQL")
