f30b323aecf61f7e5a28d3d56592acd77d016d8f
Jan Kotanski <jankotan@gmail.com>
2026-09-08 13:50:50 +0200

    fix typo

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -4,7 +4,7 @@ echo "install pixi"
 docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh ; source .sh.sh ; pixi add   numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver nxswriter pymysql nxstools pymysql sardana'
 
 echo "run nxsrecselector tests"
-ocker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation'
+docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation'
 cmd="source .sh.sh ;  python test $1"
 echo $cmd
 docker exec  ndts /bin/bash -c "$cmd"
