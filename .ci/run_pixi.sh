57fde1b6541626548c9fc80e84d028e274e10160
Jan Kotanski <jankotan@gmail.com>
2026-09-10 10:00:18 +0200

    update tests

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -5,7 +5,8 @@ docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; exp
 
 echo "run nxsrecselector tests"
 docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation'
-cmd="source .sh.sh ;  python -m unittest test.ExtraNXSRecSelector_test.ExtraNXSRecSelectorTest.test_create_init_typeshape_tango_nods_attr"
+# cmd="source .sh.sh ;  python -m unittest test.ExtraNXSRecSelector_test.ExtraNXSRecSelectorTest.test_create_init_typeshape_tango_nods_attr"
+cmd="source .sh.sh ;  python -m unittest test.ExtraNXSRecSelector_test.ExtraNXSRecSelectorTest.test_create_step_typeshape_tango_nods_attr"
 echo $cmd
 docker exec  ndts /bin/bash -c "$cmd"
 
